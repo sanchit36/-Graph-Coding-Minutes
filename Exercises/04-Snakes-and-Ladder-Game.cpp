ac120de0a2130cbb1d4735e8e681df3eaefdafde
sanchit36 <sbhadgal_be19@thapar.edu>
2021-11-03 14:14:10 +0530

    BFS

---
+++ Exercises/04-Snakes-and-Ladder-Game.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int min_dice_throws(int n, vector<pair<int, int>> snakes, vector<pair<int, int>> ladders)
+{
+}
+
+int main(int argc, char)
+{
+
+  return 0;
+}
