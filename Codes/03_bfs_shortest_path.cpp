c2241d0e7641a20a73ca8bac3b9846e0251d1e54
sanchit36 <sbhadgal_be19@thapar.edu>
2021-11-03 14:13:32 +0530

    BFS

---
+++ Codes/03_bfs_shortest_path.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <list>
+#include <queue>
+using namespace std;
+
+class Graph
+{
+  int V;
+  list<int> *l;
+
+public:
+  Graph(int v)
+  {
+    V = v;
+    l = new list<int>[V];
+  }
+
+  void addEdge(int i, int j, bool undir = true)
+  {
+    l[i].push_back(j);
+    if (undir)
+    {
+      l[j].push_back(i);
+    }
+  }
+  void bfsShortestPath(int source, int dest = -1)
+  {
+    queue<int> q;
+    bool *visited = new bool[V]{0};
+    int *dist = new int[V]{0};
+    int *parent = new int[V]{-1};
+
+    q.push(source);
+    visited[source] = true;
+    parent[source] = source;
+    dist[source] = 0;
+
+    while (!q.empty())
+    {
+      int f = q.front();
+      q.pop();
+
+      // Push the nbrs of current node into the queue
+      for (auto nbr : l[f])
+      {
+        if (!visited[nbr])
+        {
+          q.push(nbr);
+          // parent and distance
+          parent[nbr] = f;
+          dist[nbr] = dist[f] + 1;
+          visited[nbr] = true;
+        }
+      }
+    }
+
+    // print the shortest distance
+    for (int i = 0; i < V; i++)
+    {
+      cout << "shortest distance to " << i << " is " << dist[i] << endl;
+    }
+    // print the path form a source to a destination
+
+    if (dest != -1)
+    {
+      while (dest != source)
+      {
+        cout << dest << " -- ";
+        dest = parent[dest];
+      }
+      cout << source;
+    }
+  }
+};
+
+int main()
+{
+  Graph g(7);
+  g.addEdge(0, 1);
+  g.addEdge(1, 2);
+  g.addEdge(2, 3);
+  g.addEdge(3, 5);
+  g.addEdge(5, 6);
+  g.addEdge(4, 5);
+  g.addEdge(0, 4);
+  g.addEdge(3, 4);
+  g.bfsShortestPath(1, 6);
+
+  return 0;
+}
